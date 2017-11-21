07e5e39f2cc4c73c252ad3a0e8504cab235f17e2
Felix Wambiri <felixwambiri21@gmail.com>
2017-11-21 22:45:22 +0300

    [NEW] Add event attendee class model

---
+++ app/models/attendants.py
@@ -0,0 +1,13 @@
+def search():
+    """
+    This search method is work in progress
+    It is made global so that it can be used in other class models
+    :return:
+    """
+
+
+class Attendants:
+    def __init__(self, name, email, contacts):
+        self.name = name
+        self.email = email
+        self.contacts = contacts
