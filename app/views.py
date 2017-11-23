3f25666d3b9393fec4e73c6a9399de90840ff136
Felix Wambiri <felixwambiri21@gmail.com>
2017-11-23 15:08:22 +0300

    [ADD] Add the login route for our app and implement user login

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, flash, url_for
+from flask_login import LoginManager, login_user
 from werkzeug.utils import redirect
 
 from app.forms import RegisterForm
@@ -7,12 +8,49 @@ from app.models.user_acounts import UserAccounts
 
 app = Flask(__name__)
 app.secret_key = '\xcd]\x1f\x8a\xa7\xd0J\xd6\x99\x8c/\x1e\x91~hU4tgd\xe5\xa2\xab3'
+
+# Create login manager class
+login_manager = LoginManager()
+
+# Configure login
+login_manager.init_app(app)
+
+# View to be directed to for unauthorized attempt to access a protected page
+login_manager.login_view = "/"
+
+# Message flashed for unauthorized attempt to access a protected page
+login_manager.login_message = u"Please Login First to access this page"
+login_manager.login_message_category = "info"
+
+# User accounts object
 user_accounts = UserAccounts()
 
 
+# Callback method to reload the user object
+@login_manager.user_loader
+def load_user(username):
+    return user_accounts.get_specific_user(username)
+
+
 # Index route
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_f = request.form['password']
+        if user_accounts.get_specific_user(username):
+            if password_f == user_accounts.get_specific_user(username).password:
+                app.logger.info('Password Matched')
+                login_user(user_accounts.get_specific_user(username))
+                flash("You have logged in successfully", 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid Password'
+                return render_template("index.html", error=error)
+
+        else:
+            error = 'Invalid Username, The Username does not exist'
+            return render_template("index.html", error=error)
     return render_template("index.html")
 
 
@@ -22,7 +60,7 @@ def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
         if user_accounts.get_specific_user(form.username.data):
-            flash("User already exists, choose another username")
+            flash("User already exists, choose another username", 'warning')
         else:
             user = User(form.username.data, form.email.data, form.password.data, form.confirm_password.data)
             user_accounts.create_user(user)
@@ -31,7 +69,11 @@ def register():
     return render_template("register.html", form=form)
 
 
+# User dashboard route
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
