9422f2da64bcb03373d7a3ae6611d4a7d23ef4c3
Felix Wambiri <felixwambiri21@gmail.com>
2017-11-23 23:29:56 +0300

    [FIX] Add an additional attribute also added to user accounts file

--- app/tests/test_user_accounts.py
+++ app/tests/test_user_accounts.py
@@ -16,9 +16,9 @@ class TestUserAccounts(unittest.TestCase):
         Instantiating reusable variables
         """
         self.user_accounts = UserAccounts()
-        self.user1 = User("Johny", "johny@bravo.com", "johnybravobravo")
-        self.user2 = User("Fellow1", "fellow1@andela.com", "bootcampertofellow")
-        self.user3 = User("Ricky", "ricky@morty.com", "rickandmorty")
+        self.user1 = User("Johny", "johny@bravo.com", "johnybravobravo", "johnybravobravo")
+        self.user2 = User("Fellow1", "fellow1@andela.com", "bootcampertofellow", "bootcampertofellow")
+        self.user3 = User("Ricky", "ricky@morty.com", "rickandmorty","rickandmorty")
 
     # Test if user accounts has been created successfully
     def test_successful_creation_of_user_accounts(self):
@@ -49,7 +49,7 @@ class TestUserAccounts(unittest.TestCase):
 
     # Test exception is raised on registration of two users with same username/id
     def test_exception_raised_on_registration_of_two_users_using_the_same_id(self):
-        self.user4 = User("Johny", "johny@bravo.com", "johnybravobravo")
+        self.user4 = User("Johny", "johny@bravo.com", "johnybravobravo", "johnybravobravo")
         self.user_accounts.create_user(self.user1)
         self.assertRaises(KeyError, self.user_accounts.create_user, self.user4)
 
@@ -71,7 +71,7 @@ class TestUserAccounts(unittest.TestCase):
 
     # Test that an attempt to deregister a user not in existence raises an exception
     def test_deregistration_of_a_non_existent_user_raises_exception(self):
-        self.user4 = User("Johny", "johny@bravo.com", "johnybravobravo")
+        self.user4 = User("Johny", "johny@bravo.com", "johnybravobravo", "johnybravobravo")
         self.assertRaises(KeyError, self.user_accounts.delete_user, "Johny")
 
     # Test that the method get_specific_user returns that specified user
@@ -82,3 +82,5 @@ class TestUserAccounts(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+    
